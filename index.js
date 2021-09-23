320c88f6e1dcb84c03e562f63811ade08b38ce71
unknown <vaiserhyl@yandex.ru>
2021-09-23 15:26:53 +0300

    Создание сервера через веб-фраемворк Restify

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const { read } = require('fs');
@@ -7,8 +7,27 @@ const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+//app.use(express.json());
+app.use(restify.json());*/
+var restify = require('restify');
+var server = restify.createServer({
+  name: 'Hello World!',
+  version: '1.0.0'
+});
 
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.jsonp());
+server.use(restify.bodyParser({ mapParams: true }));
+
+server.get('/locations/:name', function(req, res, next){
+var name_value  = req.params.name;
+res.contentType = 'json';
+
+console.log(req.params.name_value);
+console.log(req.body.test);
+});
+
+/*
 app.use((req, res, next) => {
   console.log('URL = ', req.url);
   console.log('Original_URL = ', req.originalUrl);
@@ -21,10 +40,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/test', (req, res) => {
+pp.get('/test', (req, res) => {
   res.status(200).json({ message: 'KKKKKK'});
   
 })
+
 app.post('/test', (req, res) => {
     res.status(200).json({ message: 'FDFFFFF'});   
   })
@@ -56,3 +76,7 @@ app.post('/test', (req, res) => {
 http.createServer(app).listen(3000, () => {
   console.log('Server is working on port 3000');
 })
+*/
+server.listen(3000, function () {
+    console.log('%s listening at %s', server.name, server.url);
+  });
